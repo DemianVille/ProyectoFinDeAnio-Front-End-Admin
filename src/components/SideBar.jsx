3ee409ef5ac60942657d5fb7df2aaa778c4a4a70
DemianVille <demian111101@gmail.com>
2024-07-15 16:29:06 -0300

    asdasd

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,33 +6,35 @@ export default function SideBar() {
   return (
     <>
       <div className="sideBar body">
-        <div className="logoInfo d-flex flex-column">
-          <div className="logotipo">
-            <Image className="logoImg" src="/src/assets/Logo.png" alt="" />
+        <div className="sideBarPosition">
+          <div className="logoInfo d-flex flex-column">
+            <div className="logotipo">
+              <Image className="logoImg" src="/src/assets/Logo.png" alt="" />
+            </div>
+            <h2 className="mb-3 fontPlaywrite">Copéllia</h2>
           </div>
-          <h2 className="mb-3 fontPlaywrite">Copéllia</h2>
-        </div>
-        <div className="d-flex flex-column">
-          <Link to={"/admin"} className="w-100 adminBtnsH">
-            <button className="adminBtns my-3 p-2 w-100">Dashboard</button>
-          </Link>
-          <Link to={"/admin/categorias"} className="w-100 adminBtnsH">
-            <button className="adminBtns my-3 p-2 w-100">Categorías</button>
-          </Link>
-          <Link to={"/admin/productos"} className="w-100 adminBtnsH">
-            <button className="adminBtns my-3 p-2 w-100">Productos</button>
-          </Link>
-          <Link to={"/admin/usuarios"} className="w-100 adminBtnsH">
-            <button className="adminBtns my-3 p-2 w-100">Usuarios</button>
-          </Link>
-          <Link to={"/admin/administradores"} className="w-100 adminBtnsH">
-            <button className="adminBtns my-3 p-2 w-100">
-              Administradores
-            </button>
-          </Link>
-          <Link to={"/admin/ordenes"} className="w-100 adminBtnsH">
-            <button className="adminBtns my-3 p-2 w-100">Ordenes</button>
-          </Link>
+          <div className="d-flex flex-column">
+            <Link to={"/admin"} className="w-100 adminBtnsH">
+              <button className="adminBtns my-3 p-2 w-100">Dashboard</button>
+            </Link>
+            <Link to={"/admin/categorias"} className="w-100 adminBtnsH">
+              <button className="adminBtns my-3 p-2 w-100">Categorías</button>
+            </Link>
+            <Link to={"/admin/productos"} className="w-100 adminBtnsH">
+              <button className="adminBtns my-3 p-2 w-100">Productos</button>
+            </Link>
+            <Link to={"/admin/usuarios"} className="w-100 adminBtnsH">
+              <button className="adminBtns my-3 p-2 w-100">Usuarios</button>
+            </Link>
+            <Link to={"/admin/administradores"} className="w-100 adminBtnsH">
+              <button className="adminBtns my-3 p-2 w-100">
+                Administradores
+              </button>
+            </Link>
+            <Link to={"/admin/ordenes"} className="w-100 adminBtnsH">
+              <button className="adminBtns my-3 p-2 w-100">Ordenes</button>
+            </Link>
+          </div>
         </div>
       </div>
     </>
